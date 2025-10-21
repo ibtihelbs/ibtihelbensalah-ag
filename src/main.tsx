66fcf4067e6ca6ca718647d881fa1bd628f70a06
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-10-21 12:27:42 +0100

    csss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./app.css";
+import "./App.css";
 import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
